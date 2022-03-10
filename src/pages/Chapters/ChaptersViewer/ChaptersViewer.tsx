cbf3ec9170ce5febca18e51c0187f55615050c64
Josemagne <jaime-barakat@hotmail.com>
2022-03-10 10:49:45 +0100

    chagned ChaptersViewer, problem still exists

--- src/pages/Chapters/ChaptersViewer/ChaptersViewer.tsx
+++ src/pages/Chapters/ChaptersViewer/ChaptersViewer.tsx
@@ -12,21 +12,39 @@ type Props = {};
 const ChaptersViewer = ({}: Props) => {
   const [book, setBook] = useState<LM_Book>();
   const bookID = useSelector((state: RootState) => state.book.selectedBook);
-  //   Get the book from indexedDB
-  const _book = useLiveQuery(() => books.books.get(bookID));
-  setBook(_book);
+  console.log(bookID);
 
-  useEffect(() => {}, []);
-  return (
-    <div className="lm-chaptersviewer">
-      {book.chapters.map((chapter) => {
-        <Fragment>
-          <ChapterContainer chapter={chapter} />;
-          <ChapterAdder bookID={book.book_id} />
-        </Fragment>;
-      })}
-    </div>
-  );
+  //   Get the book from indexedDB
+  const _book = useLiveQuery(() => {
+    return books.books.get(bookID).then((res) => {
+      setBook(res);
+      console.log(book);
+      return res;
+    });
+  });
+
+  if (!_book) {
+    return (
+      <Fragment>
+        <p>No Chapters yet</p>
+        <ChapterAdder bookID={bookID} />
+      </Fragment>
+    );
+  }
+
+  console.log(book);
+
+  // useEffect(() => {}, []);
+  // return (
+  //   <div className="lm-chaptersviewer">
+  //     {_book.chapters.map((chapter) => {
+  //       <Fragment>
+  //         <ChapterContainer chapter={chapter} />;
+  //         <ChapterAdder bookID={_book.book_id} />
+  //       </Fragment>;
+  //     })}
+  //   </div>
+  // );
 };
 
 export default ChaptersViewer;
