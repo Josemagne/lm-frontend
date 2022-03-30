07fbd33e9a64b1cbb73a706511870949389c8e80
Josemagne <jaime-barakat@hotmail.com>
2022-03-30 16:37:03 +0200

    added jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     verbose: true,
     preset: "ts-jest",
-    modulePathIgnorePatterns: ["<rootDir>/lm-components/"]
+    modulePathIgnorePatterns: ["<rootDir>/lm-components/"],
+    setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"]
 };
 export default config;
